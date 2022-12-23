85a6c8172e082f209f5aa6efa3b1c50d9c5473dc
Balal Gholami <balal.gholami@gmail.com>
2022-12-23 16:12:15 +0330

    string functions

---
+++ StringFunctions.py
@@ -0,0 +1,16 @@
+course = "pytHon progRamming"
+course2 = "     pytHon progRamming    "
+course1 = course.upper()
+print(course)
+print(course1)
+courselower = course.lower()
+print(courselower)
+coursestrip = course.strip()
+print(course2)
+print(coursestrip)
+finder = course.find("H")
+print(f"index = {finder}")
+courseReplace = course.replace("m", "M")
+print(courseReplace)
+strInOrNot = "M" in course
+print(strInOrNot)
