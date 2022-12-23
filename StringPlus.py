e59b77f5118b3624ec9e6fdd8879f49223b4bf88
Balal Gholami <balal.gholami@gmail.com>
2022-12-23 10:52:30 +0330

    Formatted String

--- StringPlus.py
+++ StringPlus.py
@@ -3,3 +3,7 @@ Seperator = " "
 Last = "is cool"
 fullText = First + Seperator + Last
 print(fullText)
+# ---------
+# Formatted String: with one 'f' before "" and usint {} for formatting
+fullText2 = f"{First}    {Last}{5+9}"
+print(fullText2)
