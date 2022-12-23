9effefde7ecea04dff0dfd6e30f3141e3e20a103
Balal Gholami <balal.gholami@gmail.com>
2022-12-23 09:46:19 +0330

    Array and len

--- HelloWorld1.py
+++ HelloWorld1.py
@@ -6,4 +6,6 @@ x = 1+2
 student_count = 30
 print(student_count)
 print(2 < 4)
-course_name = "Python Programming"
+course_name = "Python Programming "
+course_length = len(course_name)
+print(course_length)
