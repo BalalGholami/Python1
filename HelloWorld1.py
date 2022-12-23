3f14474285764a543265df3003d780bf31e175b6
Balal Gholami <balal.gholami@gmail.com>
2022-12-23 09:38:06 +0330

    hello world freitag 1

--- HelloWorld1.py
+++ HelloWorld1.py
@@ -1,5 +1,9 @@
 print("hello python")
-print("*" * 5)
+print("*" * 50)
 print("new line")
 print(2 * 3)
-X = 1+2
+x = 1+2
+student_count = 30
+print(student_count)
+print(2 < 4)
+course_name = "Python Programming"
