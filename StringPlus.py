a0e109b113a67910894117a010e1c74759f8f9d7
Balal Gholami <88263085+BalalGholami@users.noreply.github.com>
2022-12-26 14:30:55 +0330

    Delete StringPlus.py

--- StringPlus.py
+++
@@ -1,9 +0,0 @@
-First = "coding"
-Seperator = " "
-Last = "is cool"
-fullText = First + Seperator + Last
-print(fullText)
-# ---------
-# Formatted String: with one 'f' before "" and usint {} for formatting
-fullText2 = f"{First}    {Last}{5+9}"
-print(fullText2)
