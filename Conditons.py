b0ff5fe874fcbf8f04fc03e7199ab7599b59aaa9
Balal Gholami <balal.gholami@gmail.com>
2022-12-27 13:55:25 +0330

    Ranges and Conditions

--- Conditons.py
+++ Conditons.py
@@ -1,1 +1,19 @@
-print ( "conditions"    )
+x = int(input("Enter your number: "))
+print(type(x))
+if x < 10:
+    print("Very cold")
+elif x < 20:
+    print("cool")
+elif x < 30:
+    print("good")
+else:
+    print("Warm")
+
+print("-------------")
+message = "warm" if x > 30 else "not warm"
+print(message)
+if 10 < x < 20:
+    message = "Normal"
+print(message)
+y = range(3)
+print(y)
