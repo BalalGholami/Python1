bcd668e0473bc64eeb6cf6aceb59a477805b45bc
Balal Gholami <balal.gholami@gmail.com>
2022-12-28 21:23:57 +0330

    listst

---
+++ lists.py
@@ -0,0 +1,12 @@
+# list
+import math
+number = 2.5
+
+letters = [1, 4, 6, 7]
+print(letters)
+letters2 = list(range(0, 20, 3))
+print(letters2)
+letters3 = list(range(0, 20))
+print(letters3)
+letters4 = (list(range(20)))
+print(letters4)
