30345a5eb0e239054cdbf1636128174bfc25b1d3
Balal Gholami <balal.gholami@gmail.com>
2022-12-28 14:03:43 +0330

    Functions and Rings

---
+++ Functions1.py
@@ -0,0 +1,16 @@
+def great():
+    print("Hello Every one")
+
+
+great()
+# -------------
+
+
+def great2(x):
+    x1 = int(x)
+    print(type(x))
+    print(type(x1))
+
+
+r = input("Enter your number: ")
+great2(r)
---
+++ Functions2.py
@@ -0,0 +1,11 @@
+def great(x, y):
+    x1 = int(x)
+    y1 = int(y)
+    r = x1/y1
+    return r
+
+
+a = input("Enter x :")
+b = input("Enter y :")
+r1 = great(x=a, y=b)
+print(r1)
